8962a76ac7638fdf49800b7742d1fd34db84e723
Ejjonzey <ejjonzey1982@gmail.com>
2017-09-11 11:54:59 -0400

    still working on nav bar links

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import AllArtists from "./components/AllArtists";
 import GlobalNav from './components/GlobalNav';
+import HomePage from './components/HomePage';
 
 
 class App extends Component {
@@ -11,7 +11,8 @@ class App extends Component {
          <div>
            
              <GlobalNav/>
-             <Route exact path='/' component={AllArtists}/>
+             <Route exact path='/' component={HomePage}/>
+             {/* <Route exact path='/' component={AllArtists}/> */}
              {/* <Route path='artist/:id' component={Artist}/> */}
         </div>
      </Router>
