d8426010d4560d59ef2db7629686da8a98e81888
Ejjonzey <ejjonzey1982@gmail.com>
2017-09-13 11:04:59 -0400

    individual artist page appearing correctly

--- client/src/components/Artist.js
+++ client/src/components/Artist.js
@@ -1,13 +1,46 @@
 import React, { Component } from 'react';
-import ArtistsCard from './ArtistsCard';
+import axios from 'axios';
+import {Link} from 'react-router-dom';
+import styled from 'styled-components';
+
+const ArtistListStyles = styled.div`
+margin: 20px 5%;
+width: 90%;
+display: flex;
+justify-content: space-around;
+flex-wrap: wrap;
+`;
 
 
 class Artist extends Component {
+    constructor(){
+        super();
+        this.state = {
+            artist: {}
+        }
+    }
+
+    componentWillMount(){
+        this._fetchArtist();
+    }
+
+    _fetchArtist = async ()=>{
+        const id = this.props.match.params.id
+        const res = await axios.get(`/api/artists/${id}`)
+        console.log(res.data)
+        this.setState({
+            artist: res.data
+        })
+    }
+
     render() {
+
         return (
-            <div>
-        
-            </div>
+            <ArtistListStyles>
+                <h1>{this.state.artist.name}</h1>
+                <img src={this.state.artist.photo_url}/>
+                <h3>{this.state.artist.description}</h3>
+            </ArtistListStyles>
         );
     }
 }
